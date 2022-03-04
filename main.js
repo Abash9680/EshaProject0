98123c9602c94a7c48c34bae08fd1935036192c3
abash9680 <abash968@gmail.com>
2022-03-04 09:30:13 -0500

    addedJs

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 console.log("is this working")
+
+// Creating a Tamagotchi class 
+// let prince = new Tamagotchi()  
+
+class Tamagotchi {
+    constructor(name, age, hunger, sleepiness, boredeom, play) {
+        this.name = name;
+        this.age = age;
+        this.hungry = hungry;
+        this.sleepy = sleepy;
+        this.boredeom = boredeom;
+        this.play = play;
+    }
+
+}
+
+let prince = new Tamagotchi("Prince", 4, 2, 0, 1, 0);
+console.log(Tamagotchi);
+
+const nameEl = document.getElementById("btn-name");
+const ageEl = document.getElementById("btn-age");
+const hungryEl = document.getElementById("btn-hungry");
+const sleepyEl = document.getElementById("btn-sleepy");
+const boredomEl = document.getElementById("btn-boredom");
+const playEl = document.getElementById("btn-play");
