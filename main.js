fae7d9a3bde1232f6b108dab8fe457ec5563a56e
abash9680 <abash968@gmail.com>
2022-03-10 17:18:59 -0500

    changes

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-console.log("is this working")
+// console.log("is this working")
 
-// Creating a Tamagotchi class 
-// let prince = new Tamagotchi()  
+// // Creating a Tamagotchi class 
+// // let prince = new Tamagotchi()  
 
-class Tamagotchi {
-    constructor(start, pause, stop, name, hungry, sleepy, bored, play) {
-        this.start = start;
-        this.pause = pause;
-        this.stop = stop;
-        this.name = name;
-        this.hungry = hungry;
-        this.sleepy = sleepy;
-        this.bored = bored;
-        this.play = play;
-        this.age = 0
-    }
-// Writing a function thats add 1 to age 
-    agePet(){ 
-      this.age = this.age + 1
-      console.log(this.age)
-    }
-    hungryPet(){
-        this.hungry = this.hungry + 1
-        console.log(this.hungry)
-    }
-}
+// class Tamagotchi {
+//     constructor(start, pause, stop, name, hungry, sleepy, bored, play) {
+//         this.start = start;
+//         this.pause = pause;
+//         this.stop = stop;
+//         this.name = name;
+//         this.hungry = hungry;
+//         this.sleepy = sleepy;
+//         this.bored = bored;
+//         this.play = play;
+//         this.age = 0
+//     }
+// // Writing a function thats add 1 to age 
+//     agePet(){ 
+//       this.age = this.age + 1
+//       console.log(this.age)
+//     }
+//     hungryPet(){
+//         this.hungry = this.hungry + 1
+//         console.log(this.hungry)
+//     }
+// }
 
-let prince = new Tamagotchi(5, 6, 5, "Prince", 4, 2, 0, 1, 0);
-console.log(Tamagotchi);
+// let prince = new Tamagotchi(5, 6, 5, "Prince", 4, 2, 0, 1, 0);
+// console.log(Tamagotchi);
 
 // letting player add in their own name for game
 getName = (e) => {
@@ -38,24 +38,22 @@ getName = (e) => {
 
 // 
 const startEl = document.getElementById("btn-start");
-const pauseEl = document.getElementById("btn-pause");
-const stopEl = document.getElementById("btn-stop");
-const nameEl = document.getElementById("btn-name");
-const hungryEl = document.getElementById("btn-hungry");
-const sleepyEl = document.getElementById("btn-sleepy");
-const boredEl = document.getElementById("btn-bored");
+const feedEl = document.getElementById("btn-feed");
+const sleepEl = document.getElementById("btn-sleep");
 const playEl = document.getElementById("btn-play");
-const ageEl = document.getElementById("age")
 
-const myCounterSt = document.getElementById("start-count")
-const myCounterPa = document.getElementById("pause-count")
-const myCounterSto = document.getElementById("stop-count")
-const myCounterNa = document.getElementById("name-count")
+
+// const hungryEl = document.getElementById("btn-hungry");
+// const sleepyEl = document.getElementById("btn-sleepy");
+// const boredEl = document.getElementById("btn-bored");
+// const playEl = document.getElementById("btn-play");
+// const ageEl = document.getElementById("age")
+
+
 const myCounterHu = document.getElementById("hungry")
 const myCounterSl = document.getElementById("sleepy")
 const myCounterBo = document.getElementById("bored")
-const myCounterPl = document.getElementById("play")
-const myCounterAg = document.getElementById("age-count")
+const myCounterAg = document.getElementById("age")
 
 // Adding in my click Event Listener for btns 
 
@@ -63,144 +61,108 @@ document.getElementById("prince").addEventListener("submit" , getName);
 
 const button = document.querySelector('.myButton');
 
+let num = 0;
+let apt = 0
+let bed = 0
+let yawn = 0
+let ageCounter = null
+let hungryCounter = null
+let sleepCounter = null
+let boredCounter = null
+
 // age to increment with start button
+// console.log("try")
 function handleStartClick() {
     ageCount()
     hungerCount()
     sleepyCount()
     boredCount()
-    playCount()
-    startCounter = setInterval(function(){
-     count++;
-     startEl.innerText = "Count: " + count;
-    },1000);
+    
 };
 //  getting hunger to increment by 1
+function ageCount() {
+    ageCounter = setInterval(function() {
+    //   increment princes age by 1
+    // prince.agePet()
+      num++;
+      myCounterAg.innerText = "age: " + num;  // change age pet to variable princes 
+      
+      }, 5000);
+}
 
 function hungerCount(){
-    hungerCounter = setInterval(function(){
+    hungryCounter = setInterval(function(){
         // incrementing hunger count
-     prince.hungryPet()   
-     count++;
-     myCounterHu.innerText = "Count: " + count;
+    apt++;
+    myCounterHu.innerText = "hungry: " + apt;
     },1000);
 }
 //  getting my sleep to increment 
 
 function sleepyCount(){
     sleepCounter = setInterval(function(){
-      count++;
-      myCounterSl.innerText = "Count: " + count; 
-    },6000);
+      bed++;
+      myCounterSl.innerText = "sleep: " + bed; 
+    },2000);
 };
 // bored funct to increment
 
 function boredCount(){
     boredCounter = setInterval(function(){
-        count++;
-        myCounterBo.innerText = "Count: " + count;
-    },6000);
+        yawn++;
+        myCounterBo.innerText = "bored: " + yawn;
+    },3000);
 };
+
+function feedClick(){
+    apt--;
+    myCounterHu.innerText = "hungry " + apt;
+}
+
+function sleepClick(){
+    bed--;
+    myCounterSl.innerText = "sleepy " + bed;
+}
+
+function playClick(){
+    yawn--;
+    myCounterBo.innerText = "bored " + yawn;
+}
+
+
 
 // play func to increment
 
-function playCount(){
-    sleepCounter = setInterval(function(){
-        count++;
-        myCounterPl.innerText = "Count: " + count;
-    },6000);
-};
+// function playCount(){
+//     sleepCounter = setInterval(function(){
+//         count++;
+//         myCounterPl.innerText = "Count: " + count;
+//     },6000);
+// };
 
 // all func will stop with pause func
 
 function handlePauseClick() {
-    clearInterval(startCounter);
-    clearInterval(ageCounter);
-    clearInterval(hungerCounter);
-    clearInterval(sleepCounter);
-    clearInterval(boredCounter);
-    clearInterval(playCounter);
-};
-  
-function handleStopClick() {
-    clearInterval(stopCounter);
-    count = 5;
-    count-- >=-1;
-    stopEl.innerText = "Count: " + count;
+    clearInterval(ageCount);
+    clearInterval(hungerCount);
+    clearInterval(sleepyCount);
+    clearInterval(boredCount);
+    // clearInterval(boredCounter);
+    // clearInterval(playCounter);
 };
 
-function ageCount() {
-    ageCounter = setInterval(function() {
-    //   increment princes age by 1
-    prince.agePet()
-    //   count++;
-      ageEl.innerText = "age: " + prince.age;  // change age pet to variable princes 
-      
-      }, 5000);
-}
 
-function handleHungryClick() {
-    
-    hungerCounter = setInterval(function(){
-        // incrementing hunger count
-     prince.hungryPet()   
-     count++;
-     hungryEl.innerText = "Count: " + count;
-    },1000);
-};
   
-  
-
-//   function handleHungryClick() {
-//     setInterval(myCounter);
+// function handleStopClick() {
+//     clearInterval(stopCounter);
 //     count = 5;
-//     hungryEl.innerText = "Count: " + count;
-//   };
-
-function handleSleepyClick() {
-    
-    sleepCounter = setInterval(function(){
-     count++;
-     sleepyEl.innerText = "Count: " + count;
-    },1000);
-};
-  
-//  
-//   function handleBoredClick() {
-//     clearInterval(myCounter);
-//     count = 5;
-//     boredEl.innerText = "Count: " + count;
-//   };
-
-  function handleBoredClick() {
-    
-    myCounter = setInterval(function(){
-     count++;
-     boredEl.innerText = "Count: " + count;
-    },1000);
-  };
-  
-  
-  
-//   function handlePlayClick() {
-//     clearInterval(myCounter);
-//     count = 5;
-//     playEl.innerText = "Count: " + count;
-//   };
-
-function handlePlayClick() {
-    
-    myCounter = setInterval(function(){
-     count++;
-     playEl.innerText = "Count: " + count;
-    },6000);
-  };
-  
-  
-
-  startEl.addEventListener('click', handleStartClick);
-  pauseEl.addEventListener('click', handlePauseClick);
-  stopEl.addEventListener('click', handleStopClick); 
+//     count-- >=-1;
+//     stopEl.innerText = "Count: " + count;
+// };
+    startEl.addEventListener('click', handleStartClick);
+    feedEl.addEventListener('click', feedClick);
+    sleepEl.addEventListener('click', sleepClick);
+    playEl.addEventListener('click', playClick); 
 //   hungryEl.addEventListener('click', handleHungryClick); 
 //   sleepyEl.addEventListener('click', handleSleepyClick); 
 //   boredEl.addEventListener('click', handleBoredClick); 
@@ -208,41 +170,41 @@ function handlePlayClick() {
 
 
 
-// adding count id's to my btns 
-const startBtn = document.getElementById("start-btn")
-// console.log(startbtn)
+// // adding count id's to my btns 
+// const startBtn = document.getElementById("start-btn")
+// // console.log(startbtn)
 
-let count = 0;
-let myCounter = null;
-console.log(myCounter);
+// let count = 0;
+// let myCounter = null;
+// console.log(myCounter);
 
-let started = false
+// let started = false
 
-function pauseBtn() {
-    if(!startBtn) {
-        myCounter = setInterval(function(){
-            num++;
-            stopMycounter()
-            myCounterSt.innerText= num;
-            started = true;
-        },1000);
-    }
-}
-function pHunger (){
-    if (num >1 ){
-        num--
-        myCounterSt.innerText= num;
-    }
+// function pauseBtn() {
+//     if(!startBtn) {
+//         myCounter = setInterval(function(){
+//             num++;
+//             stopMycounter()
+//             myCounterSt.innerText= num;
+//             started = true;
+//         },1000);
+//     }
+// }
+// function pHunger (){
+//     if (num >1 ){
+//         num--
+//         myCounterSt.innerText= num;
+//     }
 
-}
-function stopMycounter() {
-    if (num >= 10) {
-        clearInterval(myCounter)
-    }
-}
-// function pressHungryClick() {
-//     myCounter = setInterval(function(){
-//         count++;
-//         countEl.innerText = "Count: " + count;
-//     }, 1000);
-// };
+// }
+// function stopMycounter() {
+//     if (num >= 10) {
+//         clearInterval(myCounter)
+//     }
+// }
+// // function pressHungryClick() {
+// //     myCounter = setInterval(function(){
+// //         count++;
+// //         countEl.innerText = "Count: " + count;
+// //     }, 1000);
+// // };
